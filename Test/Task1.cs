5e98772bbc38396ad06f56cf793630c2ab29264c
Unknown <lojso.gm@gmail.com>
2020-11-25 02:58:13 +0300

    Update ignore

---
+++ Test/Task1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Test
+{
+    [TestFixture]
+    public class Task1
+    {
+        [Test]
+        public void CountEmpty()
+        {
+            
+        }
+    }
+}
