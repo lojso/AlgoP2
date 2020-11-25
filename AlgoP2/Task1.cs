81a7a9dcb19e5fd9009689029fac4190b22b9db7
Unknown <lojso.gm@gmail.com>
2020-11-25 13:54:22 +0300

    Тест и задача

--- AlgoP2/Task1.cs
+++ AlgoP2/Task1.cs
@@ -1,4 +1,4 @@
-﻿﻿using System;
+﻿using System;
 using System.Collections.Generic;
 
 // https://skillsmart.ru/algo/15-121-cm/z9h53ee284.html
@@ -9,15 +9,20 @@ namespace AlgorithmsDataStructures2
         public T NodeValue; // значение в узле
         public SimpleTreeNode<T> Parent; // родитель или null для корня
         public List<SimpleTreeNode<T>> Children; // список дочерних узлов или null
-	
+
         public SimpleTreeNode(T val, SimpleTreeNode<T> parent)
         {
             NodeValue = val;
             Parent = parent;
             Children = null;
         }
+
+        public override string ToString()
+        {
+            return NodeValue.ToString();
+        }
     }
-	
+
     public class SimpleTree<T>
     {
         public SimpleTreeNode<T> Root; // корень, может быть null
@@ -26,23 +31,33 @@ namespace AlgorithmsDataStructures2
         {
             Root = root;
         }
-	
+
         public void AddChild(SimpleTreeNode<T> ParentNode, SimpleTreeNode<T> NewChild)
         {
+            if (ParentNode.Children == null)
+                ParentNode.Children = new List<SimpleTreeNode<T>>();
+
             ParentNode.Children.Add(NewChild);
+            NewChild.Parent = ParentNode;
         }
 
         public void DeleteNode(SimpleTreeNode<T> NodeToDelete)
         {
             NodeToDelete.Parent.Children.Remove(NodeToDelete);
+
+            if (NodeToDelete.Children == null)
+                return;
             
-            NodeToDelete.Parent.Children.AddRange(NodeToDelete.Children);
+            foreach (var node in NodeToDelete.Children)
+            {
+                NodeToDelete.Parent.Children.Add(node);
+            }
         }
 
         public List<SimpleTreeNode<T>> GetAllNodes()
         {
             var nodes = new List<SimpleTreeNode<T>>();
-            if(Root == null)
+            if (Root == null)
                 return nodes;
             var queue = new Queue<SimpleTreeNode<T>>();
             queue.Enqueue(Root);
@@ -51,7 +66,11 @@ namespace AlgorithmsDataStructures2
                 var currentNode = queue.Dequeue();
                 nodes.Add(currentNode);
 
-                foreach (var node in queue)
+
+                if (currentNode.Children == null)
+                    continue;
+
+                foreach (var node in currentNode.Children)
                 {
                     queue.Enqueue(node);
                 }
@@ -59,22 +78,26 @@ namespace AlgorithmsDataStructures2
 
             return nodes;
         }
-	
+
         public List<SimpleTreeNode<T>> FindNodesByValue(T val)
         {
-            if(Root == null)
+            if (Root == null)
                 return null;
-            
+
             var nodes = new List<SimpleTreeNode<T>>();
             var queue = new Queue<SimpleTreeNode<T>>();
             queue.Enqueue(Root);
+
             while (queue.Count > 0)
             {
                 var currentNode = queue.Dequeue();
-                if(currentNode.Equals(val))
+                if (currentNode.NodeValue.Equals(val))
                     nodes.Add(currentNode);
 
-                foreach (var node in queue)
+                if (currentNode.Children == null)
+                    continue;
+
+                foreach (var node in currentNode.Children)
                 {
                     queue.Enqueue(node);
                 }
@@ -82,14 +105,17 @@ namespace AlgorithmsDataStructures2
 
             return nodes;
         }
-   
+
         public void MoveNode(SimpleTreeNode<T> OriginalNode, SimpleTreeNode<T> NewParent)
         {
             OriginalNode.Parent.Children.Remove(OriginalNode);
+
+            if(NewParent.Children == null)
+                NewParent.Children = new List<SimpleTreeNode<T>>();
             
             NewParent.Children.Add(OriginalNode);
         }
-   
+
         public int Count()
         {
             return GetAllNodes().Count;
@@ -97,7 +123,7 @@ namespace AlgorithmsDataStructures2
 
         public int LeafCount()
         {
-            if(Root == null)
+            if (Root == null)
                 return 0;
 
             int leafConter = 0;
@@ -106,24 +132,25 @@ namespace AlgorithmsDataStructures2
             while (queue.Count > 0)
             {
                 var currentNode = queue.Dequeue();
-                
+
                 if (currentNode.Children == null)
                 {
                     leafConter++;
+                    continue;
                 }
-                else if (currentNode.Children.Count == 0)
+
+                if (currentNode.Children.Count == 0)
                 {
                     leafConter++;
                 }
-                
-                foreach (var node in queue)
+
+                foreach (var node in currentNode.Children)
                 {
                     queue.Enqueue(node);
                 }
             }
-            
+
             return leafConter;
         }
     }
- 
 }
--- Test/Task1.cs
+++ Test/Task1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using AlgorithmsDataStructures2;
 using NUnit.Framework;
 
 namespace Test
@@ -7,9 +9,179 @@ namespace Test
     public class Task1
     {
         [Test]
-        public void CountEmpty()
+        public void CountNull()
         {
+            var tree = new SimpleTree<int>(null);
             
+            Assert.True(tree.Count() == 0);
+        }
+        
+        [Test]
+        public void Create()
+        {
+            var root = new SimpleTreeNode<int>(1, null);
+            var tree = new SimpleTree<int>(root);
+
+            Assert.True(tree.Count() == 1);
+            Assert.True(tree.FindNodesByValue(1).Count == 1);
+            Assert.True(tree.GetAllNodes().Count == 1);
+            Assert.True(tree.LeafCount() == 1);
+        }
+
+        [Test]
+        public void Add()
+        {
+            var root = new SimpleTreeNode<int>(0, null);
+            var node1 = new SimpleTreeNode<int>(1, null);
+            var node2 = new SimpleTreeNode<int>(2, null);
+            var node3 = new SimpleTreeNode<int>(3, null);
+            var node4 = new SimpleTreeNode<int>(4, null);
+            var node5 = new SimpleTreeNode<int>(5, null);
+            
+            var tree = new SimpleTree<int>(root);
+            
+            tree.AddChild(root, node1);
+            tree.AddChild(root, node2);
+            tree.AddChild(root, node3);
+            tree.AddChild(node3, node4);
+            tree.AddChild(node3, node5);
+
+            Assert.True(tree.Count() == 6);
+            Assert.True(tree.FindNodesByValue(1).Count == 1);
+            Assert.True(tree.GetAllNodes().Count == 6);
+            Assert.True(tree.LeafCount() == 4);
+        }
+        
+        [Test]
+        public void Delete()
+        {
+            var root = new SimpleTreeNode<int>(0, null);
+            var node1 = new SimpleTreeNode<int>(1, null);
+            var node2 = new SimpleTreeNode<int>(2, null);
+            var node3 = new SimpleTreeNode<int>(3, null);
+            var node4 = new SimpleTreeNode<int>(4, null);
+            var node5 = new SimpleTreeNode<int>(5, null);
+            
+            var tree = new SimpleTree<int>(root);
+            
+            tree.AddChild(root, node1);
+            tree.AddChild(root, node2);
+            tree.AddChild(root, node3);
+            tree.AddChild(node3, node4);
+            tree.AddChild(node3, node5);
+            
+            tree.DeleteNode(node5);
+            tree.DeleteNode(node2);
+
+            Assert.True(tree.Count() == 4);
+            Assert.True(tree.FindNodesByValue(1).Count == 1);
+            Assert.True(tree.GetAllNodes().Count == 4);
+            Assert.True(tree.LeafCount() == 2);
+        }
+        
+        [Test]
+        public void FindAfterDelete()
+        {
+            var root = new SimpleTreeNode<int>(0, null);
+            var node1 = new SimpleTreeNode<int>(1, null);
+            var node2 = new SimpleTreeNode<int>(2, null);
+            var node3 = new SimpleTreeNode<int>(3, null);
+            var node4 = new SimpleTreeNode<int>(4, null);
+            var node5 = new SimpleTreeNode<int>(5, null);
+            
+            var tree = new SimpleTree<int>(root);
+            
+            tree.AddChild(root, node1);
+            tree.AddChild(root, node2);
+            tree.AddChild(root, node3);
+            tree.AddChild(node3, node4);
+            tree.AddChild(node3, node5);
+            
+            Assert.True(tree.FindNodesByValue(5).Count == 1);
+            
+            tree.DeleteNode(node5);
+            tree.DeleteNode(node2);
+
+            Assert.True(tree.Count() == 4);
+            Assert.True(tree.FindNodesByValue(5).Count == 0);
+            Assert.True(tree.GetAllNodes().Count == 4);
+            Assert.True(tree.LeafCount() == 2);
+        }
+        
+        [Test]
+        public void FindMultiple()
+        {
+            var root = new SimpleTreeNode<int>(0, null);
+            var node1 = new SimpleTreeNode<int>(1, null);
+            var node2 = new SimpleTreeNode<int>(2, null);
+            var node3 = new SimpleTreeNode<int>(3, null);
+            var node4 = new SimpleTreeNode<int>(4, null);
+            var node5 = new SimpleTreeNode<int>(5, null);
+            var node5_2 = new SimpleTreeNode<int>(5, null);
+            
+            var tree = new SimpleTree<int>(root);
+            
+            tree.AddChild(root, node1);
+            tree.AddChild(root, node2);
+            tree.AddChild(root, node3);
+            tree.AddChild(node3, node4);
+            tree.AddChild(node3, node5);
+            tree.AddChild(node3, node5_2);
+
+            Assert.True(tree.Count() == 7);
+            Assert.True(tree.FindNodesByValue(5).Count == 2);
+            Assert.True(tree.GetAllNodes().Count == 7);
+            Assert.True(tree.LeafCount() == 5);
+        }
+        
+        [Test]
+        public void DeleteBranch()
+        {
+            var root = new SimpleTreeNode<int>(0, null);
+            var node1 = new SimpleTreeNode<int>(1, null);
+            var node2 = new SimpleTreeNode<int>(2, null);
+            var node3 = new SimpleTreeNode<int>(3, null);
+            var node4 = new SimpleTreeNode<int>(4, null);
+            var node5 = new SimpleTreeNode<int>(5, null);
+            var node5_2 = new SimpleTreeNode<int>(5, null);
+            
+            var tree = new SimpleTree<int>(root);
+            
+            tree.AddChild(root, node1);
+            tree.AddChild(root, node2);
+            tree.AddChild(node2, node3);
+            tree.AddChild(node2, node4);
+
+            tree.DeleteNode(node2);
+
+            Assert.True(tree.Count() == 4);
+            Assert.True(tree.GetAllNodes().Count == 4);
+            Assert.True(tree.LeafCount() == 3);
+        }
+        
+        [Test]
+        public void MoveBranch()
+        {
+            var root = new SimpleTreeNode<int>(0, null);
+            var node1 = new SimpleTreeNode<int>(1, null);
+            var node2 = new SimpleTreeNode<int>(2, null);
+            var node3 = new SimpleTreeNode<int>(3, null);
+            var node4 = new SimpleTreeNode<int>(4, null);
+            var node5 = new SimpleTreeNode<int>(5, null);
+            var node5_2 = new SimpleTreeNode<int>(5, null);
+            
+            var tree = new SimpleTree<int>(root);
+            
+            tree.AddChild(root, node1);
+            tree.AddChild(root, node2);
+            tree.AddChild(node2, node3);
+            tree.AddChild(node2, node4);
+
+            tree.MoveNode(node2, node1);
+
+            Assert.True(tree.Count() == 5);
+            Assert.True(tree.GetAllNodes().Count == 5);
+            Assert.True(tree.LeafCount() == 2);
         }
     }
 }
