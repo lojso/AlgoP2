0a7b0fb03cc38e82a44ff7fbfcbb2a9be0910659
Nikita Grishaev <grishaev@rightway.games>
2020-12-27 23:47:40 +0300

    Сделал задачу

--- AlgoP2/Task8.cs
+++ AlgoP2/Task8.cs
@@ -27,31 +27,34 @@ namespace AlgorithmsDataStructures2
 	
         public void AddVertex(int value)
         {
-            // ваш код добавления новой вершины 
-            // с значением value 
-            // в свободную позицию массива vertex
+            vertex[value] = new Vertex(value);
         }
 
         // здесь и далее, параметры v -- индекс вершины
         // в списке  vertex
         public void RemoveVertex(int v)
         {
+            vertex[v] = null;
             // ваш код удаления вершины со всеми её рёбрами
         }
 	
         public bool IsEdge(int v1, int v2)
         {
             // true если есть ребро между вершинами v1 и v2
-            return false;
+            return m_adjacency[v1, v2] != 0;
         }
 	
         public void AddEdge(int v1, int v2)
         {
+            m_adjacency[v1, v2] = 1;
+            m_adjacency[v2, v1] = 1;
             // добавление ребра между вершинами v1 и v2
         }
 	
         public void RemoveEdge(int v1, int v2)
         {
+            m_adjacency[v1, v2] = 0;
+            m_adjacency[v2, v1] = 0;
             // удаление ребра между вершинами v1 и v2
         }
     }
