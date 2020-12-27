b25db1af98672f46e5ae46ccef0f4998a40f006b
Nikita Grishaev <grishaev@rightway.games>
2020-12-27 21:08:48 +0300

    Шаблон для 8 задания

---
+++ AlgoP2/Task8.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures2
+{
+    public class Vertex
+    {
+        public int Value;
+        public Vertex(int val)
+        {
+            Value = val;
+        }
+    }
+  
+    public class SimpleGraph
+    {
+        public Vertex [] vertex;
+        public int [,] m_adjacency;
+        public int max_vertex;
+	
+        public SimpleGraph(int size)
+        {
+            max_vertex = size;
+            m_adjacency = new int [size,size];
+            vertex = new Vertex [size];
+        }
+	
+        public void AddVertex(int value)
+        {
+            // ваш код добавления новой вершины 
+            // с значением value 
+            // в свободную позицию массива vertex
+        }
+
+        // здесь и далее, параметры v -- индекс вершины
+        // в списке  vertex
+        public void RemoveVertex(int v)
+        {
+            // ваш код удаления вершины со всеми её рёбрами
+        }
+	
+        public bool IsEdge(int v1, int v2)
+        {
+            // true если есть ребро между вершинами v1 и v2
+            return false;
+        }
+	
+        public void AddEdge(int v1, int v2)
+        {
+            // добавление ребра между вершинами v1 и v2
+        }
+	
+        public void RemoveEdge(int v1, int v2)
+        {
+            // удаление ребра между вершинами v1 и v2
+        }
+    }
+}
